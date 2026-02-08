a18bb43cc2b56638298bec785350b2506cab2c25
SOR SOPHEAK <ssopheak2002@gmail.com>
2026-02-08 15:43:22 +0700

    project structure

---
+++ android/app/src/main/kotlin/com/example/flutter_product_portfolio/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_product_portfolio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_product_portfolio/features/home/presentation/screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Porfolio',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
